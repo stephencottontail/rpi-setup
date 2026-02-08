88462393c3c39b1baa561ed0f5756531b916a615
Stephen Dickinson <stephencottontail@me.com>
2026-02-08 13:42:10 -0700

    Start generating YAML

--- cloud-init/index.js
+++ cloud-init/index.js
@@ -1,9 +1,10 @@
 import wizard from './src/wizard.js'
+import generate from './src/generate.js'
 
 (async () => {
   const options = await wizard()
 
-  console.log('index', options)
+  console.log('generate', generate(options))
 })().catch((err) => {
   console.error(err)
 })
---
+++ cloud-init/src/generate.js
@@ -0,0 +1,140 @@
+const mkPasswordBlock = (userPassword) => {
+  if (userPassword) {
+    const lock_passwd = true
+    const hashed_password = userPassword // TODO
+    const password = `
+    lock_passwd: ${lock_passwd}
+    passwd: ${hashed_password}`
+
+    return password
+  } else {
+    const lock_passwd = false
+    const password = `
+    lock_passwd: ${lock_passwd}
+`
+
+    return password
+  }
+}
+
+const mkAuthorizedKeysBlock = (sshKey) => {
+  const key = sshKey // TODO: Read the file
+  const ssh_authorized_keys = `
+    ssh_authorized_keys:
+      - ${key}`
+
+  return ssh_authorized_keys
+}
+
+const mkWirelessBlock = (useStatic, useWireless, wirelessIP, router, nameservers, ssid, ssidPassword) => {
+  let useDHCP
+  let staticBlock
+
+  if (!useWireless) {
+    return ''
+  }
+
+  if (useStatic) {
+    useDHCP = false
+    staticBlock = `
+      addresses: [${wirelessIP}]
+      gateway4: ${router}
+      nameservers:
+        addresses: [${nameservers}]
+`
+  } else {
+    useDHCP = true
+    staticBlock = ''
+  }
+
+  const wireless = `
+  wifis:
+    renderer: NetworkManager
+    wlan0:
+      dhcp4: ${useDHCP}
+      regulatory-domain: "US"
+      access-points:
+        "${ssid}":
+          password: "${ssidPassword}"
+      optional: true
+      ${staticBlock}`
+
+  return wireless
+}
+
+const mkEthernetBlock = (useStatic, ethernetIP, router, nameservers) => {
+  let useDHCP
+  let staticBlock
+
+  if (useStatic) {
+    useDHCP = false
+    staticBlock = `
+      addresses: [${ethernetIP}]
+      gateway4: ${router}
+      nameservers:
+        addresses: [${nameservers}]`
+  } else {
+    useDHCP = true
+    staticBlock = ''
+  }
+
+  const ethernet = `
+  ethernets:
+    renderer: NetworkManager
+    eth0:
+      dhcp4: ${useDHCP}
+      regulatory-domain: "US"
+      ${staticBlock}`
+
+  return ethernet
+}
+
+const generate = (options) => {
+  const {
+    dryRun,
+    hostname,
+    timeZone,
+    user,
+    userPassword,
+    sshKey,
+    useStatic,
+    ethernetIP,
+    router,
+    nameservers,
+    useWireless,
+    ssid,
+    ssidPassword,
+    wirelessIP
+  } = options;
+  const userdataHeader = `#cloud-config`
+  const userdataBody = `
+hostname: ${hostname}
+manage_etc_hosts: true
+
+timezone: ${timeZone}
+
+users:
+  - name: ${user}
+    groups: users,sudo
+    shell: /bin/bash
+    ${mkPasswordBlock(userPassword)}
+    ${mkAuthorizedKeysBlock(sshKey)}
+    sudo: ALL=(ALL)
+
+enable_ssh: true
+ssh_pwauth: false`
+  const networkConfigBody = `
+network:
+  version: 2
+  ${mkWirelessBlock(useStatic, useWireless, wirelessIP, router, nameservers, ssid, ssidPassword)}
+  ${mkEthernetBlock(useStatic, ethernetIP, router, nameservers)}`
+  const output = `
+${userdataHeader}
+${userdataBody}
+===
+${networkConfigBody}`
+
+  return output
+}
+
+export default generate
